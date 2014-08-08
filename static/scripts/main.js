a573799403c99bfb98db1c89ed3a2fa09c125bf2
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-08-08 16:48:17 -0500

    Work on MediaSource stuff

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -10,4 +10,45 @@ require([
 function ($, _, Backbone,
     Stream, StreamCollection, Segment, SegmentCollection) {
 
+  window.MediaSource = window.MediaSource || window.WebKitMediaSource;
+
+  var GET = function (url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.responseType = 'arraybuffer';
+    xhr.send();
+
+    xhr.onload = function(e) {
+      if (xhr.status != 200) {
+        alert("Unexpected status code " + xhr.status + " for " + url);
+        return false;
+      }
+      callback(new Uint8Array(xhr.response));
+    };
+  };
+
+  var getSegment = function (segmentId, sourceBuffer, callback) {
+    var segmentURL = '/streams/stream-2/segments/' + segmentId + '.webm';
+    //var segmentURL = '/test.webm';
+    GET(segmentURL, function (uInt8Array) {
+      callback(uInt8Array);
+    });
+  };
+
+  $(function () {
+    var video = $('video').get(0);
+    var mediaSource = new window.MediaSource();
+    video.src = window.URL.createObjectURL(mediaSource);
+
+    mediaSource.addEventListener('webkitsourceopen', function (e) {
+      var sourceBuffer = mediaSource.addSourceBuffer(
+          'video/webm; codecs="vorbis,vp8"');
+
+      // add segments to the video element
+      getSegment(0, sourceBuffer, function (segmentData) {
+        console.log(mediaSource);
+        sourceBuffer.append(segmentData);
+      });
+    });
+  });
 });
